3c9ae5f401fe430bd84951b07e7d881dfc4d115e
hcsmart <hcsmart6417@gmail.com>
2026-08-27 11:29:04 +0900

    Add files via upload

--- ki_config.js
+++ ki_config.js
@@ -4,7 +4,7 @@
 ============================================================ */
 const KI_CFG = {
   APP_NAME : 'KI MES',
-  VER      : 'v9.0',
+  VER      : 'v9.1',
   SUPABASE_URL : 'https://ipggvrzxfcryzryileuv.supabase.co',
   SUPABASE_KEY : 'sb_publishable_CHO-dAOU00HNwno52255mg_H3C1_vew',
   DB_PREFIX    : 'ki_',
